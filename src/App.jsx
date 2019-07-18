56d6808ee9448ff61401647d49e6ac1bbf9a3605
Esteban Sanchez <41240399+esanchezvz@users.noreply.github.com>
2019-07-17 19:18:24 -0500

    added header component with basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/home-page.component';
 import ShopPage from './pages/shop/shop-page.component';
+import Header from './components/header/header.component';
 
 const App = () => {
   return (
     <React.Fragment>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
