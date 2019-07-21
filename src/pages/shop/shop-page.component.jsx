d12a8f8e2136417e41bf377eaecad0c23468031d
Esteban Sanchez <41240399+esanchezvz@users.noreply.github.com>
2019-07-21 18:51:32 -0500

    created shop reducer and selectors updating corresponding components, optimizing data flow

--- src/pages/shop/shop-page.component.jsx
+++ src/pages/shop/shop-page.component.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router-dom';
 
-export class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectiosOverview from '../../components/collections-overview/collectios-overview.component';
+import CollectionPage from '../collection/collection-page.component';
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => {
+  return (
+    <div className='shop-page'>
+      <Route exact path={match.path} component={CollectiosOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collection`}
+        component={CollectionPage}
+      />
+    </div>
+  );
+};
 
 export default ShopPage;
